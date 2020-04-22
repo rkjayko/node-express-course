592f336ee1279f39f6ccaf1a840ee14fb9a3d0b7
Jaime David Mejia Quintero <jmejia@cidenet.com.co>
2020-04-22 09:37:30 -0500

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 
+//return json data
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
+
 //bottom lines
-app.listen(8000,function(){
-    console.log("server is running");
-    });
+app.listen(8000,function(){console.log('server is listening')});
